aeac6d50093a910776c4ea8418ad5305e2636be9
Zach Ross <zrross11@gmail.com>
2025-12-20 16:21:41 -0500

    nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,83 @@
 -- Neovim Configuration
 -- Basic configuration for a clean, functional setup
 
+-- Install lazy.nvim plugin manager if not already installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup plugins
+require("lazy").setup({
+  -- Catppuccin colorscheme
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = "latte",
+          dark = "mocha",
+        },
+        transparent_background = false,
+        show_end_of_buffer = false,
+        term_colors = false,
+        dim_inactive = {
+          enabled = false,
+          shade = "dark",
+          percentage = 0.15,
+        },
+        no_italic = false,
+        no_bold = false,
+        no_underline = false,
+        styles = {
+          comments = { "italic" },
+          conditionals = { "italic" },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+        },
+        color_overrides = {},
+        custom_highlights = {},
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = "",
+          },
+        },
+      })
+      
+      -- Set colorscheme
+      vim.cmd.colorscheme "catppuccin"
+    end,
+  },
+})
+
 -- Basic settings
 vim.opt.number = true           -- Show line numbers
-vim.opt.relativenumber = true   -- Show relative line numbers
+vim.opt.clipboard = "unnamedplus"
 vim.opt.expandtab = true        -- Use spaces instead of tabs
 vim.opt.shiftwidth = 2          -- Size of an indent
 vim.opt.tabstop = 2             -- Number of spaces tabs count for
